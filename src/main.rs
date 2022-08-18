27fe1b5467e74a24febbf61392fd107668bdcc60
YeahNotSewerSide <47860375+DoctorEenot@users.noreply.github.com>
2022-08-18 21:06:53 +0300

    added --resize parameter

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
-struct Args{
+struct Args {
     #[clap(short, long, value_parser)]
     tiles: String,
 
@@ -14,30 +14,28 @@ struct Args{
     output: String,
 
     #[clap(short, long, value_parser, default_value_t = 20)]
-    width:u32,
+    width: u32,
 
     #[clap(short, long, value_parser, default_value_t = 20)]
-    height:u32
+    height: u32,
 
+    #[clap(short, long, value_parser)]
+    resize: bool
 }
 
-
 fn main() {
-    
     let args = Args::parse();
 
-    let kernel_size = (args.width,args.height);
+    let kernel_size = (args.width, args.height);
 
-    let tiles = mosaic::prepare_tiles(
-        &args.tiles, 
-        kernel_size).unwrap();
+    let tiles = mosaic::prepare_tiles(&args.tiles, kernel_size).unwrap();
 
 
-    let image = mosaic::build_mosaic(
-        &args.source, 
-        &tiles, 
-        kernel_size
-    ).unwrap();
-    
+    let image = if args.resize{
+        mosaic::build_mosaic(&args.source, &tiles, kernel_size).unwrap()
+    }else{
+        mosaic::build_mosaic_without_compression(&args.source, &tiles, kernel_size).unwrap()
+    };
+
     image.save(args.output).unwrap();
 }
